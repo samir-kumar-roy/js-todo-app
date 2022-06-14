933ee214ca93d6e741f7f7be6fda0ecac2619785
Samir Kumar Roy <samirroy594@gmail.com>
2022-06-14 11:26:59 +0600

    Now the app working perfectly..

--- index.js
+++ index.js
@@ -1,51 +1,96 @@
 // console.log("Hello");
-const toDoList = document.querySelector(".todo-list");
-let undoneBtns = document.querySelectorAll(".undone-btn");
-let doneBtns = document.querySelectorAll(".done-btn");
-let newTodoItem = document.createElement("div");
-newTodoItem.classList.add("todo-item");
-let newTodoPara = document.createElement("p");
-newTodoPara.textContent = "Programming Practice";
-newTodoItem.append(newTodoPara);
+let toDoList = document.querySelector(".todo-list");
 
-let buttons = document.createElement("div");
-buttons.classList.add("buttons");
+// let newTodoItem = document.createElement("div");
+// newTodoItem.classList.add("todo-item");
+// let newTodoPara = document.createElement("p");
+// newTodoPara.textContent = "Programming Practice";
+// newTodoItem.append(newTodoPara);
 
-let doneUndoneWrappper = document.createElement("div");
+// let buttons = document.createElement("div");
+// buttons.classList.add("buttons");
 
-const donebutton = document.createElement("a");
-donebutton.classList.add("done-btn", "btn");
-donebutton.setAttribute("href", "#");
-donebutton.textContent = "Done";
-doneUndoneWrappper.append(donebutton);
+// let doneUndoneWrappper = document.createElement("div");
 
-const unDonebutton = document.createElement("a");
-unDonebutton.classList.add("undone-btn", "btn");
-unDonebutton.setAttribute("href", "#");
-unDonebutton.textContent = "Undone";
-doneUndoneWrappper.append(unDonebutton);
+// const donebutton = document.createElement("a");
+// donebutton.classList.add("done-btn", "btn");
+// donebutton.setAttribute("href", "#");
+// donebutton.textContent = "Done";
+// doneUndoneWrappper.append(donebutton);
 
-buttons.append(doneUndoneWrappper);
+// const unDonebutton = document.createElement("a");
+// unDonebutton.classList.add("undone-btn", "btn");
+// unDonebutton.setAttribute("href", "#");
+// unDonebutton.textContent = "Undone";
+// doneUndoneWrappper.append(unDonebutton);
 
-const deletebutton = document.createElement("a");
-deletebutton.classList.add("delete-btn", "btn");
-deletebutton.setAttribute("href", "#");
-deletebutton.textContent = "Delete";
-buttons.append(deletebutton);
-// appending buttons onto the new todo item
+// buttons.append(doneUndoneWrappper);
 
-newTodoItem.append(buttons);
-toDoList.append(newTodoItem);
-console.log(newTodoItem);
-// for hiding the undone button when page loads
-console.log("length", undoneBtns.length)
-console.log("length of Done buttons", doneBtns.length)
+// const deletebutton = document.createElement("a");
+// deletebutton.classList.add("delete-btn", "btn");
+// deletebutton.setAttribute("href", "#");
+// deletebutton.textContent = "Delete";
+// buttons.append(deletebutton);
+// // appending buttons onto the new todo item
+
+// newTodoItem.append(buttons);
+// toDoList.append(newTodoItem);
+// console.log(newTodoItem);
+// // for hiding the undone button when page loads
+// console.log("length", undoneBtns.length)
+// console.log("length of Done buttons", doneBtns.length)
 
 
-// for (let doneBtn of doneBtns) {
-//     doneBtn.addEventListener("click", (e) => {
-//         e.preventDefault();
-//         e.target.style.display = "none";
-//         e.target.nextElementSibling.style.display = "inlineBlock";
-//     })
-// }
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", e => {
+    e.preventDefault();
+    const todoTask = document.querySelector("#todo-task").value;
+    if (todoTask == "") {
+        setTimeout(() => {
+            document.querySelector(".empty-alert").style.display = "block";
+        }, 100)
+    } else {
+
+        toDoList.innerHTML += `
+<div class="todo-item">
+            <p>${todoTask}</p>
+            <div class="buttons">
+                <div>
+                    <a href="#" class="done-btn btn">Done</a>
+                    <a href="#" class="undone-btn btn">Undone</a>
+                </div>
+                <a href="#" class="delete-btn btn">Delete</a>
+            </div>
+        </div>
+`
+    }
+    let doneBtns = document.querySelectorAll(".done-btn");
+    for (let doneBtn of doneBtns) {
+        doneBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            e.target.style.display = "none";
+            e.target.parentElement.parentElement.previousElementSibling.style.textDecoration = "line-through";
+            e.target.nextElementSibling.style.visibility = "visible";
+        })
+    }
+
+    let undoneBtns = document.querySelectorAll(".undone-btn");
+    for (let undonebutton of undoneBtns) {
+        undonebutton.addEventListener("click", function (e) {
+            e.preventDefault();
+            e.target.style.visibility = "hidden";
+            e.target.parentElement.parentElement.previousElementSibling.style.textDecoration = "none";
+            e.target.previousElementSibling.style.display = "inline-block";
+        })
+    }
+    let deleteBtns = document.querySelectorAll(".delete-btn");
+    console.log(deleteBtns);
+    for (let deletebtn of deleteBtns) {
+        deletebtn.addEventListener("click", e => {
+            e.preventDefault();
+            e.target.parentElement.parentElement.remove();
+        });
+    }
+})
+
+
